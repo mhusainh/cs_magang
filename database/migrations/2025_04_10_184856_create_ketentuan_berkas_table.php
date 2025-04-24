eaca57f93baf0cc231fad21749c9a0ae8b969ec5
mhusainh <husainhasbulah@gmail.com>
2025-04-25 01:09:35 +0700

    refactor(database): add softDeletes to ketentuan_berkas table
    Add softDeletes column to the ketentuan_berkas table to enable soft deletion functionality, improving data management and retention practices.

--- database/migrations/2025_04_10_184856_create_ketentuan_berkas_table.php
+++ database/migrations/2025_04_10_184856_create_ketentuan_berkas_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('jenjang_sekolah');
             $table->integer('is_required');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
