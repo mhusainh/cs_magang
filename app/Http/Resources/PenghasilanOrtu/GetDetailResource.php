5180a4875156dff34f91fba6b27f7023eef927aa
mhusainh <husainhasbulah@gmail.com>
2025-04-30 09:28:29 +0700

    feat: add 'id' field to GetDetailResource response
    Include the 'id' field in the response of GetDetailResource to provide the unique identifier of the resource, ensuring complete data is returned to the client.

--- app/Http/Resources/PenghasilanOrtu/GetDetailResource.php
+++ app/Http/Resources/PenghasilanOrtu/GetDetailResource.php
@@ -15,6 +15,7 @@ class GetDetailResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
             'penghasilan_ortu' => $this->penghasilan_ortu
         ];
     }
