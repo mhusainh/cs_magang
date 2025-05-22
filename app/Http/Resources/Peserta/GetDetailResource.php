10db304b0525dbd30e9342b2992ee3b9cd23ae5b
mhusainh <husainhasbulah@gmail.com>
2025-05-22 21:39:18 +0700

    feat: add asal_sekolah field to GetDetailResource
    This commit introduces the 'asal_sekolah' field to the GetDetailResource to include the participant's school of origin in the response. This change ensures that all relevant participant details are provided in the API response.

--- app/Http/Resources/Peserta/GetDetailResource.php
+++ app/Http/Resources/Peserta/GetDetailResource.php
@@ -21,6 +21,7 @@ class GetDetailResource extends JsonResource
             'tempat_lahir' => $this->tempat_lahir,
             'tanggal_lahir' => $this->tanggal_lahir,
             'alamat' => $this->alamat,
+            'asal_sekolah' => $this->asal_sekolah,
             'jurusan1' => $this->jurusan1 ? [
                 'id' => $this->jurusan1->id,
                 'jurusan' => $this->jurusan1->jurusan,
